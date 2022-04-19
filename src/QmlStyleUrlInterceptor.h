fad1fcf955353eb5d27a72e390fb0f8285d19673
Uwe Kindler <uwe_kindler@web.de>
2022-04-19 14:41:30 +0200

    Added missing license information to source files

--- src/QmlStyleUrlInterceptor.h
+++ src/QmlStyleUrlInterceptor.h
@@ -1,5 +1,24 @@
 #ifndef ACSS_CQMLSTYLEURLINTERCEPTOR_H
 #define ACSS_CQMLSTYLEURLINTERCEPTOR_H
+/*******************************************************************************
+** Qt Advanced Stylesheets
+** Copyright (C) 2022 Uwe Kindler
+**
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Lesser General Public
+** License as published by the Free Software Foundation; either
+** version 2.1 of the License, or (at your option) any later version.
+**
+** This library is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+** Lesser General Public License for more details.
+**
+** You should have received a copy of the GNU Lesser General Public
+** License along with this library; If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
+
 //============================================================================
 /// \file   QmlStyleUrlInterceptor.h
 /// \author Florian Meinicke (florian.meinicke@t-online.de)
