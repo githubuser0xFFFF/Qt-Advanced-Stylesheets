a90eff10c9f36256281fecad95bbfbfe77a13acd
Uwe Kindler <uwe_kindler@web.de>
2022-08-12 10:58:21 +0200

    Fixed crash when using Qt6 with MSVC

--- examples/full_features/main.cpp
+++ examples/full_features/main.cpp
@@ -1,10 +1,14 @@
 #include <QApplication>
+#include <QQuickWindow>
 
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+    QQuickWindow::setGraphicsApi(QSGRendererInterface::OpenGL);
+#endif
     CMainWindow w;
     w.show();
     return a.exec();
